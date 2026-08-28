492c996d8c58aae5f37273cf05b0d78858266ba5
karthik270307 <karthikeyanmurugesan2710@gmail.com>
2026-08-28 13:40:35 +0530

    Update projectRoutes.js

--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const projectController = require("../controllers/projectController");
+const projectController = require("../controllers/projectcontroller");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const campaignController = require("../controllers/campaignController");
